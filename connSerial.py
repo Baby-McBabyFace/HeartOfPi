799d24148ba11085b27e26790e9715682d246ea1
Baby-McBabyFace <32679064+Baby-McBabyFace@users.noreply.github.com>
2023-01-30 17:53:11 +0800

    Update connSerial.py

--- connSerial.py
+++ connSerial.py
@@ -8,10 +8,19 @@ class Serial:
         
     def send_command(self, command):
         command = str.encode(command)
+        print(type(command))
         self.ser.write(command)
         
     def receive_command(self):
         return self.ser.readline().decode().strip()
+    
+    def send_int_to_byte_command(self, command):
+        command = int.to_bytes(command, 8, 'big')
+        print(type(command))
+        self.ser.write(command)
+    
+    def receive_int_to_byte_command(self):
+        return self.ser.readline()
     
     def close(self):
         self.ser.close()
