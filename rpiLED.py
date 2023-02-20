229561ff9fb29ccc7057abb3634712bcb3b7ab81
C210150 <muttlt25@gmail.com>
2023-02-20 17:14:58 +0800

    Add LEDs

--- rpiLED.py
+++ rpiLED.py
@@ -2,8 +2,15 @@
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(23, GPIO.OUT)
-GPIO.output(23, GPIO.HIGH)
-time.sleep(1)
-GPIO.output(23, GPIO.LOW)
+class myLED:
+    def __init__(self, gpioPin):
+        self.gpioPin = gpioPin
+        gpio.setmode(GPIO.BCM)
+        GPIO.setup(self.gpioPin, GPIO.OUT)
+
+    def up(self):
+        GPIO.output(self.gpioPin, GPIO.HIGH)
+        
+    def down(self):
+        GPIO.output(self.gpioPin, GPIO.LOW)
+        
